cad327c3b867328c15cb4947489c0aee7fffe2d0
Pas <askpeevee@gmail.com>
2023-06-15 22:41:43 +0100

    State Tasks

--- my-pas-app/src/App.jsx
+++ my-pas-app/src/App.jsx
@@ -4,25 +4,25 @@
 // Include that component in App.jsx = Done
 // Please un comment code before you can run them based on individual homework
 
-import React, { Component } from 'react';
-import Elements from './components/Elements.jsx';
-import Shopping from "./components/Shopping.jsx"
-import Cars from './components/Cars.jsx';
+// import React, { Component } from 'react';
+// import Elements from './components/Elements.jsx';
+// import Shopping from "./components/Shopping.jsx"
+// import Cars from './components/Cars.jsx';
 
-class App extends Component {
+// class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <Elements />
-        <Shopping />
-        <Cars />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <Elements />
+//         <Shopping />
+//         <Cars />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
 
 // Pass a prop into the component at #2 = Done
 
@@ -43,22 +43,4 @@ export default App;
 
 // export default App;
 
-// Add some state to the App component
 
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   state = { count: 0 };
-
-//   onClick = () => {
-//     this.setState({ count: this.state.count + 1 });
-//   };
-
-//   render() {
-
-//     return<p onClick={this.onClick}>{this.state.count}</p>
-
-//   }
-// }
-
-// export default App;
---
+++ my-pas-app/src/components/IsChild.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class IsChild extends Component {
+  
+    render() { 
+        console.log(this.props);
+        return ();
+    }
+}
+ 
+export default IsChild;
